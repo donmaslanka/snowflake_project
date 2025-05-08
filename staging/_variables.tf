60fed4406e4cccbe5328b8b871b27e172a9a7381
Don Maslanka <dmaslanka@gmail.com>
2025-05-08 10:17:51 -0400

    added iam code s3 backend code

--- staging/_variables.tf
+++ staging/_variables.tf
@@ -13,3 +13,28 @@ variable "snowflake_private_key" {
   description = "Private key used to access Snowflake"
   sensitive   = true
 }
+
+variable "snowflake_username" {
+  type    = string
+  default = ""
+}
+variable "snowflake_password" {
+  type        = string
+  description = "value of the password used to access Snowflake"
+  sensitive   = true
+  # This is a sensitive variable, so it should not be hardcoded in the code.
+  default = ""
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "S3 bucket name for storing Terraform state"
+  default     = "tuai_remote_state"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region for resources"
+  default     = "us-west-2"
+}
+
